8f6b03a5fd4015b63306b7a7d888c4670fc0d60d
Pietrzi <pietrzi@op.pl>
2019-10-14 11:57:02 +0200

    half layout

--- src/components/Home/HomeFourSteps/HomeFourSteps.js
+++ src/components/Home/HomeFourSteps/HomeFourSteps.js
@@ -2,8 +2,35 @@ import React from 'react'
 
 export default function HomeFourSteps() {
     return (
-        <div>
-            HomeFourSteps
+        <div className="homefoursteps__wrapper">
+            <p className="homefoursteps__title">Wystarczą 4 proste kroki</p>
+            <div className="homefoursteps__svg"></div>
+            <div className="homefoursteps__columns__wrapper">
+                <div className="homefoursteps__column">
+                    <div className="column__svg1"></div>
+                    <p className="column__title">Wybierz rzeczy</p>
+                    <hr></hr>
+                    <div className="column__text">ubrania, zabawki, sprzęt i inne</div>
+                </div>
+                <div className="homefoursteps__column">
+                    <div className="column__svg2"></div>
+                    <p className="column__title">Spakuj je</p>
+                    <hr></hr>
+                    <div className="column__text">skorzystaj z worków na śmieci</div>
+                </div>
+                <div className="homefoursteps__column">
+                    <div className="column__svg3"></div>
+                    <p className="column__title">Zdecyduj komu<br></br>chcesz pomóc</p>
+                    <hr></hr>
+                    <div className="column__text">wybierz zaufane miejsce</div>
+                </div>
+                <div className="homefoursteps__column">
+                    <div className="column__svg4"></div>
+                    <p className="column__title">Zamów kuriera</p>
+                    <hr></hr>
+                    <div className="column__text">kurier przyjedzie w dogodnym momencie</div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Home/HomeThreeColumns/HomeThreeColumns.js
+++ src/components/Home/HomeThreeColumns/HomeThreeColumns.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function HomeThreeColumns() {
     return (
-        <div>
-            HomeThreeColumns
+        <div className="homethreecolumns__wrapper">
+            <div className="column__wrapper">
+                <p className="big__number">5</p>
+                <p className="column__title">oddanych worków</p>
+                <p className="column__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum placeat magni voluptas accusantium optio temporibus iure similique obcaecati!</p>
+            </div>
+            <div className="column__wrapper">
+                <p className="big__number">10</p>
+                <p className="column__title">wspartych organizacji</p>
+                <p className="column__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum placeat magni voluptas accusantium optio temporibus iure similique obcaecati!</p>
+            </div>
+            <div className="column__wrapper">
+                <p className="big__number">7</p>
+                <p className="column__title">zorganizowanych zbiórek</p>
+                <p className="column__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum placeat magni voluptas accusantium optio temporibus iure similique obcaecati!</p>
+            </div>
         </div>
     )
 }
