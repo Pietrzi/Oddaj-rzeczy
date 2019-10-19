0706f476fa26d59daeb5bde07bd4dd8006cbc8a9
Pietrzi <pietrzi@op.pl>
2019-10-19 17:43:28 +0200

    moved back to show project

--- src/components/Home/HomeContact/HomeContact.js
+++ src/components/Home/HomeContact/HomeContact.js
@@ -1,37 +1,74 @@
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 
-export default function HomeContact() {
-    return (
-        <div className="contact__wrapper" id="contact">
-            <div className="contact__layer">
-                <div className="form__wrapper">
-                    <p className="form__title">Skontaktuj się z nami</p>
-                    <div className="form__svg"></div>
-                    <form>
-                        <div className="form__el">
-                            <p>Wpisz swoje imię</p>
-                            <input type="text" name="name" placeholder="Krzysztof"/>
+class HomeContact extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         option: 'fundacje',
+    //         fundacje: [
+    //             {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'},
+    //             {name: 'Fundacja "Dla dzieci"', target: 'Cel i misja: Pomoc dzieciom z ubogich rodzin', what: 'ubrania, meble, zabawki'},
+    //             {name: 'Fundacja "Bez domu"', target: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania', what: 'ubrania, jedzenie, ciepłe koce'},
+    //             {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'},
+    //             {name: 'Fundacja "Dla dzieci"', target: 'Cel i misja: Pomoc dzieciom z ubogich rodzin', what: 'ubrania, meble, zabawki'},
+    //             {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'},
+    //             {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'},
+    //             {name: 'Fundacja "Bez domu"', target: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania', what: 'ubrania, jedzenie, ciepłe koce'},
+    //             {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'}
+    //         ],
+    //         organizacje: [
+    //             {name: 'Organizacja Lorem 1', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+    //             {name: 'Organizacja Lorem 2', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+    //             {name: 'Organizacja Lorem 3', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+    //             {name: 'Organizacja Lorem 4', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+    //             {name: 'Organizacja Lorem 5', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+    //             {name: 'Organizacja Lorem 6', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'}
+    //         ],
+    //         lokalne: [
+    //             {name: 'Zbiórka Lorem 1', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+    //             {name: 'Zbiórka Lorem 2', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+    //             {name: 'Zbiórka Lorem 3', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'}
+    //         ]
+    //     }
+    // }
+
+    render() {
+        return (
+            <div className="contact__wrapper" id="contact">
+                <div className="contact__layer">
+                    <div className="form__wrapper">
+                        <p className="form__title">Skontaktuj się z nami</p>
+                        <div className="form__svg"></div>
+                        <form>
+                            <div className="form__el">
+                                <p>Wpisz swoje imię</p>
+                                <input type="text" name="name" placeholder="Krzysztof"/>
+                            </div>
+                            <div className="form__el">
+                                <p>Wpisz swój email</p>
+                                <input type="text" name="email" placeholder="abc@def.pl"/>
+                            </div >
+                            <div className="form__textarea">
+                                <p>Wpisz swoją wiadomość</p>
+                                <textarea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."></textarea>
+                            </div>
+                            <input className="form__button" type="submit" value="Wyślij"/>
+                        </form>
+                    </div>
+                    <div className="bottom__contact">
+                        <p>Copyright by Coders Lab</p>
+                        <div className="bottom__icons">
+                            <i className="fa fa-facebook-square"></i>
+                            <i className="fa fa-instagram"></i>
                         </div>
-                        <div className="form__el">
-                            <p>Wpisz swój email</p>
-                            <input type="text" name="email" placeholder="abc@def.pl"/>
-                        </div >
-                        <div className="form__textarea">
-                            <p>Wpisz swoją wiadomość</p>
-                            <textarea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."></textarea>
-                        </div>
-                        <input className="form__button" type="submit" value="Wyślij"/>
-                    </form>
-                </div>
-                <div className="bottom__contact">
-                    <p>Copyright by Coders Lab</p>
-                    <div className="bottom__icons">
-                        <i className="fa fa-facebook-square"></i>
-                        <i className="fa fa-instagram"></i>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
+
+
+export default HomeContact;
--- src/components/Home/HomeHelp/HomeHelp.js
+++ src/components/Home/HomeHelp/HomeHelp.js
@@ -1,50 +1,105 @@
-import React from 'react'
+import React from 'react';
 
-export default function HomeHelp() {
-    return (
-        <div className="homehelp__wrapper" id="help">
-            <p className="help__title">Komu pomagamy?</p>
-            <div className="help__svg"></div>
-            <ul className="ul__titles">
-                <li>Fundacjom</li>
-                <li>Organizacjom<br></br>pozarządowym</li>
-                <li>Lokalnym<br></br>zbiórkom</li>
-            </ul>
-            <p className="help__text">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
-            <div className="help__box">
-                <div className="one__box">
-                    <div className="left__box">
-                        <p className="box__title">Fundacja "Dbam o Zdrowie"</p>
-                        <p className="box__left__text">Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej</p>
+class HomeHelp extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            option: 'fundacje',
+            fundacje: [
+                {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'},
+                {name: 'Fundacja "Dla dzieci"', target: 'Cel i misja: Pomoc dzieciom z ubogich rodzin', what: 'ubrania, meble, zabawki'},
+                {name: 'Fundacja "Bez domu"', target: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania', what: 'ubrania, jedzenie, ciepłe koce'},
+                {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'},
+                {name: 'Fundacja "Dla dzieci"', target: 'Cel i misja: Pomoc dzieciom z ubogich rodzin', what: 'ubrania, meble, zabawki'},
+                {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'},
+                {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'},
+                {name: 'Fundacja "Bez domu"', target: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania', what: 'ubrania, jedzenie, ciepłe koce'},
+                {name: 'Fundacja "Dbam o Zdrowie"', target: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej', what: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'}
+            ],
+            organizacje: [
+                {name: 'Organizacja Lorem 1', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+                {name: 'Organizacja Lorem 2', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+                {name: 'Organizacja Lorem 3', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+                {name: 'Organizacja Lorem 4', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+                {name: 'Organizacja Lorem 5', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+                {name: 'Organizacja Lorem 6', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'}
+            ],
+            lokalne: [
+                {name: 'Zbiórka Lorem 1', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+                {name: 'Zbiórka Lorem 2', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'},
+                {name: 'Zbiórka Lorem 3', target: 'jasf sdfsiufh  sdkfs skdfj skj', what: 'lijsf, lskfofff, iasi, oiwef'}
+            ]
+        }
+    }
+
+    render() {
+        
+        // const option = this.state.option;
+        // const body = () => {
+        //     this.state.fundacje.map((el, i) => {
+        //         return (
+        //             <div className="one__box" key={ i }>
+        //                 <div className="left__box">
+        //                     <p className="box__title">{ el.name }</p>
+        //                     <p className="box__left__text">{ el.target }</p>
+        //                 </div>
+        //                 <div className="right__box">
+        //                 <p className="box__right__text">{ el.what }</p>
+        //                 </div>
+        //             </div>
+        //         )
+        //     })
+        // }
+        return (
+            <div className="homehelp__wrapper" id="help">
+                <p className="help__title">Komu pomagamy?</p>
+                <div className="help__svg"></div>
+                <ul className="ul__titles">
+                    <li>Fundacjom</li>
+                    <li>Organizacjom<br></br>pozarządowym</li>
+                    <li>Lokalnym<br></br>zbiórkom</li>
+                </ul>
+                <p className="help__text">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
+                <div className="help__box">
+                    {/* { body } */}
+                    <div className="one__box">
+                        <div className="left__box">
+                            <p className="box__title">Fundacja "Dbam o Zdrowie"</p>
+                            <p className="box__left__text">Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej</p>
+                        </div>
+                        <div className="right__box">
+                        <p className="box__right__text">ubrania, jedzenie, sprzęt AGD, meble, zabawki</p>
+                        </div>
                     </div>
-                    <div className="right__box">
-                    <p className="box__right__text">ubrania, jedzenie, sprzęt AGD, meble, zabawki</p>
+                    <div className="one__box">
+                        <div className="left__box">
+                            <p className="box__title">Fundacja "Dla dzieci"</p>
+                            <p className="box__left__text">Cel i miasja: Pomoc dzieciom z ubogich rodzin</p>
+                        </div>
+                        <div className="right__box">
+                        <p className="box__right__text">ubrania, meble, zabawki</p>
+                        </div>
+                    </div>
+                    <div className="one__box">
+                        <div className="left__box">
+                            <p className="box__title">Fundacja "Bez domu"</p>
+                            <p className="box__left__text">Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania</p>
+                        </div>
+                        <div className="right__box">
+                        <p className="box__right__text">ubrania, jedzenie, ciepłe koce</p>
+                        </div>
                     </div>
                 </div>
-                <div className="one__box">
-                    <div className="left__box">
-                        <p className="box__title">Fundacja "Dla dzieci"</p>
-                        <p className="box__left__text">Cel i miasja: Pomoc dzieciom z ubogich rodzin</p>
-                    </div>
-                    <div className="right__box">
-                    <p className="box__right__text">ubrania, meble, zabawki</p>
-                    </div>
-                </div>
-                <div className="one__box">
-                    <div className="left__box">
-                        <p className="box__title">Fundacja "Bez domu"</p>
-                        <p className="box__left__text">Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania</p>
-                    </div>
-                    <div className="right__box">
-                    <p className="box__right__text">ubrania, jedzenie, ciepłe koce</p>
-                    </div>
-                </div>
+                <ul className="ul__numbers">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
             </div>
-            <ul className="ul__numbers">
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
-    )
+        )
+    }
+    
 }
+
+
+export default HomeHelp;
