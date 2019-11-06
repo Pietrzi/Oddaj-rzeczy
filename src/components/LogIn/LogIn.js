37a12fa3928a60b15f4fd89a8517bc818c772737
Pietrzi <pietrzi@op.pl>
2019-11-06 18:22:49 +0100

    added validation

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,26 +1,84 @@
 import React from 'react'
 
 class LogIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            wrongEmail: '',
+            wrongPass: '',
+            verification: false
+        }
+    }
+
+    handleChange = e => {
+        const name = e.target.name;
+        this.setState({
+            [name]: e.target.value
+        });
+        e.target.value = ""
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+ 
+        this.validate();
+        const isValid = this.validate();
+        if(isValid) {
+            this.setState({
+                wrongEmail: '',
+                wrongPass: ''
+            })
+        }
+    }
+
+    validate = () => {
+
+            const patterns = {
+                email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+                password: /^.{6,}$/
+        };
+
+        let wrongEmail = "";
+        let wrongPass = "";
+
+        const { email, password } = this.state;
+
+        if (!patterns.email.test(email)) {
+            wrongEmail = "Podany email jest nieprawidłowy!";
+            this.setState({ wrongEmail })
+            return false;
+        }
+
+        if (!patterns.password.test(password)) {
+            wrongPass = "Hasło musi mieć conajmniej 6 znaków!";
+            this.setState({ wrongPass })
+            return false;
+        }
+
+        return true;
+    }
 
     render() {
         return (
             <div className="login__content">
-                <p className="login__title">Założ konto</p>
+                <p className="login__title">Zaloguj się</p>
                 <div className="svg"></div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form__el">
                         <p className="labels">Email</p>
-                        <input/>
-                        {/* <p className="error">{this.state.nameError}</p> */}
+                        <input onChange={this.handleChange} type="text" name="email" value={this.state.email}/>
+                        <p className="error">{this.state.wrongEmail}</p>
                     </div>
                     <div className="form__el">
                         <p className="labels">Hasło</p>
-                        <input/>
-                        {/* <p className="error">{this.state.emailError}</p> */}
+                        <input onChange={this.handleChange} type="password" name="password" value={this.state.password}/>
+                        <p className="error">{this.state.wrongPass}</p>
                     </div >
+                    <button className="button login ">Załóż konto</button>
+                    <input type="submit" className="button signin" value="Zalogój się"/>
                 </form>
-                <button className="login">Załóż konto</button>
-                <button className="signin">Zaloguj się</button>
             </div>
         )
     }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,31 +1,99 @@
 import React from 'react'
 
 class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password1: '',
+            password2: '',
+            wrongEmail: '',
+            wrongPass1: '',
+            wrongPass2: '',
+            verification: false
+        }
+    }
+
+    handleChange = e => {
+        const name = e.target.name;
+        this.setState({
+            [name]: e.target.value
+        });
+        e.target.value = ""
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+ 
+        this.validate();
+        const isValid = this.validate();
+        if(isValid) {
+            this.setState({
+                wrongEmail: '',
+                wrongPass1: '',
+                wrongPass2: ''
+            })
+        }
+    }
+
+    validate = () => {
+
+            const patterns = {
+                email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+                password: /^.{6,}$/
+        };
+
+        let wrongEmail = "";
+        let wrongPass1 = "";
+        let wrongPass2 = ""
+
+        const { email, password1, password2 } = this.state;
+
+        if (!patterns.email.test(email)) {
+            wrongEmail = "Podany email jest nieprawidłowy!";
+            this.setState({ wrongEmail })
+            return false;
+        }
+
+        if (!patterns.password.test(password1)) {
+            wrongPass1 = "Hasło musi mieć conajmniej 6 znaków!";
+            this.setState({ wrongPass1 })
+            return false;
+        }
+
+        if (password2 !== password1) {
+            wrongPass2 = "Hasła muszą być takie same";
+            this.setState({ wrongPass2 })
+            return false;
+        }
+
+        return true;
+    }
 
     render() {
         return (
             <div className="register__content">
                 <p className="register__title">Założ konto</p>
                 <div className="svg"></div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form__el">
                         <p className="labels">Email</p>
-                        <input/>
-                        {/* <p className="error">{this.state.nameError}</p> */}
+                        <input onChange={this.handleChange} type="text" name="email" value={this.state.email}/>
+                        <p className="error">{this.state.wrongEmail}</p>
                     </div>
                     <div className="form__el">
                         <p className="labels">Hasło</p>
-                        <input/>
-                        {/* <p className="error">{this.state.emailError}</p> */}
+                        <input onChange={this.handleChange} type="password" name="password1" value={this.state.password1}/>
+                        <p className="error">{this.state.wrongPass1}</p>
                     </div >
                     <div className="form__el">
                         <p className="labels">Powtórz hasło</p>
-                        <input/>
-                        {/* <p className="error">{this.state.emailError}</p> */}
+                        <input onChange={this.handleChange} type="password" name="password2" value={this.state.password2}/>
+                        <p className="error">{this.state.wrongPass2}</p>
                     </div >
+                    <button className="button login">Zaloguj się</button>
+                    <input type="submit" className="button signin" value="Załóż konto"/>
                 </form>
-                <button className="login">Zaloguj się</button>
-                <button className="signin">Załóż konto</button>
             </div>
         )
     }
