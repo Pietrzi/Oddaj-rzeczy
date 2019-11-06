7a1d8083483b754d1a39e558b350b537d9259a8b
Pietrzi <pietrzi@op.pl>
2019-11-06 10:30:24 +0100

    added logout

--- src/components/LogOut/LogOut.js
+++ src/components/LogOut/LogOut.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function LogOut() {
     return (
-        <div>
-            LogOut
+        <div className="logout__content">
+            <p>Wylogowanie nastąpiło pomyślnie!</p>
+            <div className="svg"></div>
+            <Link to="/" className="logout__button">Strona główna</Link>
         </div>
     )
 }
