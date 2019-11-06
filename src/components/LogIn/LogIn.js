d216570183d79bb17c4fb3b8aed001bebda6d1cf
Pietrzi <pietrzi@op.pl>
2019-11-06 11:41:21 +0100

    added sign/log layouts

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
-export default function LogIn() {
-    return (
-        <div>
-            LogIn
-        </div>
-    )
+class LogIn extends React.Component {
+
+    render() {
+        return (
+            <div className="login__content">
+                <p className="login__title">Założ konto</p>
+                <div className="svg"></div>
+                <form>
+                    <div className="form__el">
+                        <p className="labels">Email</p>
+                        <input/>
+                        {/* <p className="error">{this.state.nameError}</p> */}
+                    </div>
+                    <div className="form__el">
+                        <p className="labels">Hasło</p>
+                        <input/>
+                        {/* <p className="error">{this.state.emailError}</p> */}
+                    </div >
+                </form>
+                <button className="login">Załóż konto</button>
+                <button className="signin">Zaloguj się</button>
+            </div>
+        )
+    }
+    
 }
+
+export default LogIn;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
-export default function Register() {
-    return (
-        <div>
-            Register
-        </div>
-    )
+class Register extends React.Component {
+
+    render() {
+        return (
+            <div className="register__content">
+                <p className="register__title">Założ konto</p>
+                <div className="svg"></div>
+                <form>
+                    <div className="form__el">
+                        <p className="labels">Email</p>
+                        <input/>
+                        {/* <p className="error">{this.state.nameError}</p> */}
+                    </div>
+                    <div className="form__el">
+                        <p className="labels">Hasło</p>
+                        <input/>
+                        {/* <p className="error">{this.state.emailError}</p> */}
+                    </div >
+                    <div className="form__el">
+                        <p className="labels">Powtórz hasło</p>
+                        <input/>
+                        {/* <p className="error">{this.state.emailError}</p> */}
+                    </div >
+                </form>
+                <button className="login">Zaloguj się</button>
+                <button className="signin">Załóż konto</button>
+            </div>
+        )
+    }
+    
 }
+
+export default Register;
+
+// onChange={this.handleChange} type="text" name="name" placeholder="Krzysztof" value={this.state.name}
+// onChange={this.handleChange} type="text" name="email" placeholder="abc@def.pl" value={this.state.email}
