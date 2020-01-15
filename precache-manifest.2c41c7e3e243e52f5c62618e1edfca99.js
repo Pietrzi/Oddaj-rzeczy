ffe2290f2e041eff87a9ac2c5b8e8459c0407faa
Pietrzi <pietrzi@op.pl>
2020-01-15 13:53:54 +0100

    Updates

---
+++ precache-manifest.2c41c7e3e243e52f5c62618e1edfca99.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624b795a8e1f76915edf71790b7f287f",
+    "url": "/Oddaj-rzeczy/index.html"
+  },
+  {
+    "revision": "89bb97bf3a8144cd4cdc",
+    "url": "/Oddaj-rzeczy/static/css/2.c114a28c.chunk.css"
+  },
+  {
+    "revision": "29ca74268d19d78201e3",
+    "url": "/Oddaj-rzeczy/static/css/main.69783dce.chunk.css"
+  },
+  {
+    "revision": "89bb97bf3a8144cd4cdc",
+    "url": "/Oddaj-rzeczy/static/js/2.4e29613f.chunk.js"
+  },
+  {
+    "revision": "d0cb0a244b3563727e54b0f5d5982212",
+    "url": "/Oddaj-rzeczy/static/js/2.4e29613f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29ca74268d19d78201e3",
+    "url": "/Oddaj-rzeczy/static/js/main.61b901df.chunk.js"
+  },
+  {
+    "revision": "e1d89c3c6db64c5a8af8",
+    "url": "/Oddaj-rzeczy/static/js/runtime-main.b78ec139.js"
+  },
+  {
+    "revision": "7db36abd712c228ea96e6206440a9eb3",
+    "url": "/Oddaj-rzeczy/static/media/3 Columns Background.7db36abd.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/Oddaj-rzeczy/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/Oddaj-rzeczy/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/Oddaj-rzeczy/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "21ed9357304916b10044256ebb533532",
+    "url": "/Oddaj-rzeczy/static/media/Header-Form-Background.21ed9357.png"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/Oddaj-rzeczy/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/Oddaj-rzeczy/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/Oddaj-rzeczy/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/Oddaj-rzeczy/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/Oddaj-rzeczy/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/Oddaj-rzeczy/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/Oddaj-rzeczy/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Oddaj-rzeczy/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Oddaj-rzeczy/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Oddaj-rzeczy/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Oddaj-rzeczy/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Oddaj-rzeczy/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0847cec3849ed50328ca26344309d265",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-300.0847cec3.eot"
+  },
+  {
+    "revision": "2fd4ab7d19f79e1cf674b61cf6a1e14d",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-300.2fd4ab7d.svg"
+  },
+  {
+    "revision": "b1158cfcd4aacb9d8fb61625e37af46a",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-300.b1158cfc.woff2"
+  },
+  {
+    "revision": "bbe2a65480ca94b807b1e6de5941331f",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-300.bbe2a654.ttf"
+  },
+  {
+    "revision": "cc7de05e166e90320d7d896e0f72a19d",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-300.cc7de05e.woff"
+  },
+  {
+    "revision": "0eb8cb4044906e26998b710fedd6f9fd",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-700.0eb8cb40.ttf"
+  },
+  {
+    "revision": "56a90d4915cf0d39ababd6fab16295ce",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-700.56a90d49.svg"
+  },
+  {
+    "revision": "ba56ea84b8084b7ff9677f50d3cd81bd",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-700.ba56ea84.woff"
+  },
+  {
+    "revision": "c3f050ee5dce54cd208cf04924ecfd31",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-700.c3f050ee.eot"
+  },
+  {
+    "revision": "fa534be7ffa380e39a7f6e03bf9a5e03",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-700.fa534be7.woff2"
+  },
+  {
+    "revision": "00bd6529dced674aa38c5c3d643084e9",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-italic.00bd6529.svg"
+  },
+  {
+    "revision": "029f17fd20aa3756481d840f78dc3112",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-italic.029f17fd.ttf"
+  },
+  {
+    "revision": "3a9be9ea9f7aa4af6de7307df21d9fc0",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-italic.3a9be9ea.woff2"
+  },
+  {
+    "revision": "57c4a98bf4c4c335b4ddcb351020a4b3",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-italic.57c4a98b.eot"
+  },
+  {
+    "revision": "d76079ed7541a433a54f79316de086e9",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-italic.d76079ed.woff"
+  },
+  {
+    "revision": "02c60749ae5136e41f85f282d41c21e4",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-regular.02c60749.eot"
+  },
+  {
+    "revision": "69f09800f4f6479d06e44eba837df872",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-regular.69f09800.woff"
+  },
+  {
+    "revision": "8276fdb72ae8f4714d4e6eba704cc39f",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-regular.8276fdb7.woff2"
+  },
+  {
+    "revision": "94669602ffe5f3591b48aa8fb14ac180",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-regular.94669602.svg"
+  },
+  {
+    "revision": "9d98a5b844642817ab89f7d479689c00",
+    "url": "/Oddaj-rzeczy/static/media/merriweather-v21-latin-regular.9d98a5b8.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-300.27ef0b06.svg"
+  },
+  {
+    "revision": "2d01beb2d7cdc80d03f30b2bd3acbaa7",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-300.2d01beb2.eot"
+  },
+  {
+    "revision": "9342ba03760bb74ef7507ecd0a92e749",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-300.9342ba03.ttf"
+  },
+  {
+    "revision": "d2c6a4b3918b50c5f1854bb9c5d1de0e",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-300.d2c6a4b3.woff"
+  },
+  {
+    "revision": "f3f1593860f828caac8d594f23416f9c",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-300.f3f15938.woff2"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-700.2e00b263.svg"
+  },
+  {
+    "revision": "4baea1b0fd7c64811f0f5d19a84f2898",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-700.4baea1b0.ttf"
+  },
+  {
+    "revision": "5717b08cf679657595d0383d291a04c3",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-700.5717b08c.woff2"
+  },
+  {
+    "revision": "709906cfb07bf6f0bc35393b4846fafc",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-700.709906cf.eot"
+  },
+  {
+    "revision": "7f0bc4d2d95eb471662f447f10acad2e",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-700.7f0bc4d2.woff"
+  },
+  {
+    "revision": "04e02df10a43f5f01291d08b997110db",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-italic.04e02df1.woff"
+  },
+  {
+    "revision": "3ac737a839cf8a8f5106a5dad2eecd5f",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-italic.3ac737a8.woff2"
+  },
+  {
+    "revision": "5cbf63dd6edc00fb4dfed03474beda44",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-italic.5cbf63dd.eot"
+  },
+  {
+    "revision": "fb71dc3047c24b6bf2133cbe5d112795",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-italic.fb71dc30.ttf"
+  },
+  {
+    "revision": "fd2ba7503f70aafcdcc45211a481684d",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-italic.fd2ba750.svg"
+  },
+  {
+    "revision": "6035727c9748b7f19151ed3946cb8882",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-regular.6035727c.eot"
+  },
+  {
+    "revision": "615065f99828a8f1423c48ce21ddfffb",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-regular.615065f9.ttf"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "b7b7c77b83e9d67f6756aa2716f35eba",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-regular.b7b7c77b.woff"
+  },
+  {
+    "revision": "be21e759f9abd15b8cef68d92cedc081",
+    "url": "/Oddaj-rzeczy/static/media/open-sans-v17-latin_latin-ext-regular.be21e759.woff2"
+  }
+]);
